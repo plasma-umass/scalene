cd9fa9d818fb936d6fdf9c448348ce67cc000a88
emeryberger <emery.berger@gmail.com>
2019-12-17 16:00:02 -0500

    Added.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='scale',
+      version='0.1',
+      description='Scale: A Profiler for Python',
+      url='https://github.com/emeryberger/scale',
+      author='Emery Berger',
+      author_email='emery@cs.umass.edu',
+      license='Apache License 2.0',
+      packages=find_packages()
+)
