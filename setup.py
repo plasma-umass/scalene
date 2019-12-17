86c957a0e31729bfc6ec9010cb1cb5b98201cd52
emeryberger <emery.berger@gmail.com>
2019-12-17 16:00:41 -0500

    More.

--- setup.py
+++ setup.py
@@ -7,5 +7,10 @@ setup(name='scale',
       author='Emery Berger',
       author_email='emery@cs.umass.edu',
       license='Apache License 2.0',
+          classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: Apache License 2.0",
+         "Operating System :: OS Independent",
+     ],
       packages=find_packages()
 )
