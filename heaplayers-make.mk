b670eb3fad7e21c2dcdd32c535afb06e20770a1c
emeryberger <emery.berger@gmail.com>
2019-12-25 18:32:56 -0500

    Moved stuff to include dir.

--- heaplayers-make.mk
+++ heaplayers-make.mk
@@ -1,7 +1,7 @@
 CPPFLAGS = -std=c++14 -g -DNDEBUG -O3 -ffast-math -fno-builtin-malloc # -Wall -Wextra -Wshadow -Wconversion -Wuninitialized
 CXX = clang++
 
-INCLUDES = -I. -IHeap-Layers -IHeap-Layers/utility
+INCLUDES = -I. -I./include -IHeap-Layers -IHeap-Layers/utility
 MACOS_SRC = lib$(LIBNAME).cpp Heap-Layers/wrappers/macwrapper.cpp
 MACOS_COMPILE = $(CXX) -ftemplate-depth=1024 -arch x86_64 -pipe -g $(CPPFLAGS) $(INCLUDES) -D_REENTRANT=1 -compatibility_version 1 -current_version 1 -D'CUSTOM_PREFIX(x)=xx\#\#x' $(MACOS_SRC) -dynamiclib -install_name $(DESTDIR)$(PREFIX)/lib$(LIBNAME).dylib -o lib$(LIBNAME).dylib -ldl -lpthread 
 
