8ac47212e45f91bfa5492c0679c970ba34f13c03
emeryberger <emery.berger@gmail.com>
2019-12-25 18:50:55 -0500

    Now only trigger signal on GROWTH, not mere allocation activity.

--- include/sampleheap.hpp
+++ include/sampleheap.hpp
@@ -9,7 +9,9 @@ public:
 
   SampleHeap()
     : _timer (Start),
-      _signalsEnabled (false)
+      _signalsEnabled (false),
+      _mallocs (0),
+      _frees (0)
   {
     //    fprintf(stderr, "HELLO\n");
   }
@@ -23,24 +25,35 @@ public:
   }
   
   void * malloc(size_t sz) {
-    if (sz >= _timer) {
+    if (!_signalsEnabled && (sz < _timer)) {
+      _timer -= sz;
+    } else {
+      _signalsEnabled = true; // next time we will signal.
+    }
+    auto ptr = SuperHeap::malloc(sz);
+    _mallocs += SuperHeap::getSize(ptr);
+    if (_mallocs - _frees > Bytes) {
       if (_signalsEnabled) {
 	// Raise a signal.
 	//	tprintf::tprintf("freed memory = @\n", SuperHeap::freedMemory());
 	raise(SIGVTALRM);
-      } else {
-	_signalsEnabled = true; // next time we will signal.
       }
-      // Reset the counter.
-      _timer = Bytes;
-    } else {
-      _timer -= sz;
+      _mallocs = 0;
+      _frees = 0;
     }
-    return SuperHeap::malloc(sz);
+    return ptr;
+  }
+
+  void free(void * ptr) {
+    auto sz = SuperHeap::getSize(ptr);
+    _frees += sz;
+    SuperHeap::free(ptr);
   }
   
 private:
   long _timer;
+  unsigned long _mallocs;
+  unsigned long _frees;
   bool _signalsEnabled;
 };
 
