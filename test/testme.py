0da229d2e2fe9e78ee7f70c441d94f54caaa2697
emeryberger <emery.berger@gmail.com>
2019-12-29 20:29:51 -0500

    Some more tweaks.

--- test/testme.py
+++ test/testme.py
@@ -2,15 +2,17 @@ import os
 import numpy as np
 #import math
 
-#from numpy import linalg as LA
+from numpy import linalg as LA
+
+arr = [i for i in range(1,1000)]
 
 def doit1(x):
 #    x = [i*i for i in range(1,1000)][0]
     y = 1
-    # w, v = LA.eig(np.diag((1, 2, 3, 4, 5, 6, 7, 8, 9, 10)))
-    x = [i*i for i in range(0,10000)][9999]
-    y1 = [i*i for i in range(0,20000)][19999]
-    z1 = [i*i for i in range(0,30000)][29999]
+#    w, v = LA.eig(np.diag(arr)) # (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)))
+    x = [i*i for i in range(0,100000)][99999]
+    y1 = [i*i for i in range(0,200000)][199999]
+    z1 = [i for i in range(0,300000)][299999]
     z = x * y
 #    z = np.multiply(x, y)
     return z
@@ -19,11 +21,13 @@ def doit2(x):
     i = 0
 #    zarr = [math.cos(13) for i in range(1,100000)]
 #    z = zarr[0]
+    z = 0.1
     while i < 100000:
 #        z = math.cos(13)
 #        z = np.multiply(x,x)
 #        z = np.multiply(z,z)
 #        z = np.multiply(z,z)
+        z = z * z
         z = x * x
         z = z * z
         z = z * z
@@ -44,7 +48,6 @@ def stuff():
     x = 1.01
     for i in range(1,10):
         for j in range(1,10):
-            #print("WO")
             x = doit1(x)
             x = doit2(x)
             x = doit3(x)
