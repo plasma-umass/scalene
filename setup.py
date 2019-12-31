1e6471f5a887be0f3f7261ce18085f54760b564f
emeryberger <emery.berger@gmail.com>
2019-12-31 13:12:35 -0500

    Added description foo.

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
 setup(name='scalene',
-      version='0.3.1',
+      version='0.3.3',
       description='Scalene: A high-resolution, low-overhead CPU and memory profiler for Python',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/emeryberger/scalene',
       author='Emery Berger',
       author_email='emery@cs.umass.edu',
