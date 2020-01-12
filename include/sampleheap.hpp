ef9a3a0a468ffdbd272c0a7ea7509912a9e7f8c7
emeryberger <emery.berger@gmail.com>
2020-01-12 14:56:20 -0600

    Added alignment, changed to longs.

--- include/sampleheap.hpp
+++ include/sampleheap.hpp
@@ -9,6 +9,8 @@ template <unsigned long SamplingRateBytes, class SuperHeap>
 class SampleHeap : public SuperHeap {
 public:
 
+  enum { Alignment = SuperHeap::Alignment };
+  
   SampleHeap()
     : _mallocs (0),
       _frees (0)
@@ -23,11 +25,10 @@ public:
     _mallocs += SuperHeap::getSize(ptr);
     if (unlikely(_mallocs >= SamplingRateBytes)) {
       // Raise a signal.
-      //      tprintf::tprintf("signal!\n");
+      // tprintf::tprintf("signal!\n");
       //       tprintf::tprintf("SampleHeap::malloc(@) = @\n", SuperHeap::getSize(ptr), ptr);
       raise(SIGVTALRM);
-      // _mallocs -= SamplingRateBytes;
-      _mallocs = 0; 
+      _mallocs = 0;
     }
     return ptr;
   }
@@ -40,14 +41,13 @@ public:
     if (unlikely(_frees >= SamplingRateBytes)) {
       // tprintf::tprintf("SampleHeap::free @ = @\n", ptr, sz);
       raise(SIGXCPU);
-      // _frees -= SamplingRateBytes;
       _frees = 0; // -= SamplingRateBytes;
     }
   }
   
 private:
-  unsigned long _mallocs;
-  unsigned long _frees;
+  long _mallocs;
+  long _frees;
 };
 
 #endif
