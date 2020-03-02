58c8279d4aa4ed5bac49deb72e3d1750e4c36487
emeryberger <emery.berger@gmail.com>
2020-03-01 20:13:11 -0500

    Added.

---
+++ scalene/reservoir.py
@@ -0,0 +1,27 @@
+import math
+import random
+
+class reservoir():
+
+    sample_array = []
+    total_samples = 0
+    max_samples = 0
+    
+    def __init__(self, size=0):
+        self.max_samples = size
+        self.total_samples = 0
+        self.sample_array = []
+
+    def add(self, value):
+        self.total_samples += 1
+        if self.total_samples <= self.max_samples:
+            self.sample_array.append(value)
+        else:
+            assert self.max_samples == len(self.sample_array)
+            p = random.randint(0, self.total_samples - 1)
+            if p < self.max_samples:
+                self.sample_array[p] = value
+
+    def get(self):
+        return self.sample_array
+    
