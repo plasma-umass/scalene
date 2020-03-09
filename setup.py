5e2a1e13cf78b75038070497ba23884521ca5a3e
emeryberger <emery.berger@gmail.com>
2020-03-09 11:24:04 -0400

    Bumped version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
     
 setup(name='scalene',
-      version='0.7.11',
+      version='0.7.12',
       description='Scalene: A high-resolution, low-overhead CPU and memory profiler for Python',
       long_description=long_description,
       long_description_content_type='text/markdown',
