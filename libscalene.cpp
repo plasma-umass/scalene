b3295124f9aeb113b79287ee9da9c0015db46dd1
emeryberger <emery.berger@gmail.com>
2020-03-28 07:44:11 -0400

    Added copy sampling and reporting.

--- libscalene.cpp
+++ libscalene.cpp
@@ -1,5 +1,6 @@
 #include <heaplayers.h>
 
+#include "stprintf.h"
 #include "tprintf.h"
 #include "common.hpp"
 
@@ -21,16 +22,22 @@
 #include <unistd.h>
 
 
+#if defined(__APPLE__)
+#include "macinterpose.h"
+#include "tprintf.h"
+#endif
+
 class TheCustomHeap;
 static TheCustomHeap * theCustomHeap = nullptr;
 
 // We use prime numbers here (near 1MB, for example) to avoid the risk
 // of stride behavior interfering with sampling.
 
-//const auto MallocSamplingRate = 1048583;
+const auto MallocSamplingRate = 1048583UL;
+const auto MemcpySamplingRate = MallocSamplingRate * 2;
 //const auto MallocSamplingRate = 4194319;
 //const auto MallocSamplingRate = 8388617;
-const auto MallocSamplingRate = 16777259;
+//const auto MallocSamplingRate = 16777259;
 const auto RepoSize = 4096;
 
 typedef SampleHeap<MallocSamplingRate, RepoMan<RepoSize>> CustomHeapType;
@@ -49,6 +56,81 @@ TheCustomHeap& getTheCustomHeap() {
   static TheCustomHeap thang;
   return thang;
 }
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h> // for getpid()
+#include <signal.h>
+
+template <unsigned long MemcpySamplingRateBytes>
+class MemcpySampler {
+  enum { MemcpySignal = SIGPROF };
+  static constexpr auto flags = O_WRONLY | O_CREAT | O_SYNC | O_APPEND; // O_TRUNC;
+  static constexpr auto perms = S_IRUSR | S_IWUSR;
+  static constexpr auto fname = "/tmp/scalene-memcpy-signalXXXXX";
+public:
+  MemcpySampler()
+    : _interval (MemcpySamplingRateBytes),
+      _memcpyOps (0),
+      _memcpyTriggered (0)
+  {
+    signal(MemcpySignal, SIG_IGN);
+    auto pid = getpid();
+    int i;
+    for (i = 0; i < strlen(fname); i++) {
+      if (fname[i] == 'X') {
+	break;
+      }
+      scalene_memcpy_signal_filename[i] = fname[i];
+    }
+    stprintf::stprintf((char *) &scalene_memcpy_signal_filename[i], "@", pid);
+  }
+
+  ~MemcpySampler() {
+    unlink(scalene_memcpy_signal_filename);
+  }
+
+  ATTRIBUTE_ALWAYS_INLINE inline void * memcpy(void * dst, const void * src, size_t n) {
+    auto result = ::memcpy(dst, src, n);
+    _memcpyOps += n;
+    if (unlikely(_memcpyOps >= _interval)) {
+      writeCount();
+      _memcpyTriggered++;
+      _memcpyOps = 0;
+      raise(MemcpySignal);
+    }
+    return result; // always dst
+  }
+  
+private:
+  
+  unsigned long _memcpyOps;
+  unsigned long long _memcpyTriggered;
+  unsigned long _interval;
+  char scalene_memcpy_signal_filename[255];
+
+  void writeCount() {
+    char buf[255];
+    stprintf::stprintf(buf, "@,@\n", _memcpyTriggered, _memcpyOps);
+    int fd = open(scalene_memcpy_signal_filename, flags, perms);
+    write(fd, buf, strlen(buf));
+    close(fd);
+  }
+};
+
+extern "C" ATTRIBUTE_EXPORT void * xxmemcpy(void * dst, const void * src, size_t n) {
+  static MemcpySampler<MemcpySamplingRate> msamp;
+  //  tprintf::tprintf("E");
+  auto result = msamp.memcpy(dst, src, n);
+  //  tprintf::tprintf("F\n");
+  return result;
+}
+
+#if defined(__APPLE__)
+MAC_INTERPOSE(xxmemcpy, memcpy);
+#endif
 
 extern "C" ATTRIBUTE_EXPORT __attribute__((always_inline)) void * xxmalloc(size_t sz) {
   void * ptr = nullptr;
