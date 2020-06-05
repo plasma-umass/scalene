f0730c9b6f06c7fc33539bb6945435bfbd0ba4f7
emeryberger <emery.berger@gmail.com>
2020-06-05 10:03:47 -0400

    Initial.

---
+++ include/open_addr_hashtable.hpp
@@ -0,0 +1,112 @@
+#pragma once
+
+/**
+ * open_addr_hashtable: simple hash table that does no allocation.
+ * Emery Berger
+ * https://emeryberger.com
+ **/
+
+
+template <int Size>
+class open_addr_hashtable {
+public:
+  open_addr_hashtable()
+  {
+    static_assert((Size & (Size - 1)) == 0,
+		  "Size must be a power of two.");
+  }
+
+  void * get(void * k) {
+    auto ind = find(k);
+    if (ind == -1) {
+      return nullptr;
+    } else {
+      return payload[ind].value;
+    }
+  }
+
+  void put(void * k, void * v) {
+    auto h = hash1(k);
+    if (payload[h].key == nullptr) {
+      payload[h].key = k;
+    }
+    if (payload[h].key == k) {
+      payload[h].value = v;
+      return;
+    }
+    while (true) {
+      h = (h + hash2((void *) h)) & ~(Size - 1);
+      if (payload[h].key == nullptr) {
+	payload[h].key = k;
+      }
+      if (payload[h].key == k) {
+	payload[h].value = v;
+	return;
+      }
+    }
+  }
+
+  // @return true iff the element was deleted.
+  bool remove(void * k) {
+    auto h = hash1(k);
+    if (payload[h].key == nullptr) {
+      // Not in the hash table.
+      return false;
+    }
+    if (payload[h].key == k) {
+      payload[h].key = nullptr;
+      payload[h].value = nullptr;;
+      return true;
+    }
+    while (true) {
+      h = (h + hash2((void *) h)) & ~(Size - 1);
+      if (payload[h].key == nullptr) {
+	return false;
+      }
+      if (payload[h].key == k) {
+	payload[h].key = nullptr;
+	payload[h].value = nullptr;
+	return true;
+      }
+    }
+  }
+
+private:
+  // Returns -1 if not found.
+  int find(void * k) {
+    auto h = hash1(k);
+    if (payload[h].key == k) {
+      return h;
+    }
+    if (payload[h].key == nullptr) {
+      return -1;
+    }
+    while (true) {
+      h = (h + hash2((void *) h)) & ~(Size - 1);
+      if (payload[h].key == k) {
+	return h;
+      }
+      if (payload[h].key == nullptr) {
+	return -1;
+      }
+    }
+  }
+  unsigned long hash1(void * addr) {
+    auto u = (uintptr_t) addr;
+    return (u ^ 0xcafebabe) & (Size - 1);
+  }
+  unsigned long hash2(void * addr) {
+    auto u = (uintptr_t) addr;
+    return (u ^ 0xdeadbeef) & (Size - 1);
+  }
+  struct payload_t {
+    payload_t()
+      : key (nullptr),
+	value (nullptr)
+    {}
+    void * key;
+    void * value;
+  };
+  payload_t payload[Size];
+};
+
