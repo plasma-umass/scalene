71983a1310808b7c1acac48611d59c8837b2e580
emeryberger <emery.berger@gmail.com>
2020-07-19 17:47:44 -0400

    Added.

---
+++ include/sampler.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cmath>
+#include <iostream>
+#include <thread>
+#include <unistd.h>
+
+#include "mwc.h"
+
+#define SAMPLER_DETERMINISTIC 0
+
+template <int SAMPLE_RATE>
+class Sampler {
+private:
+  int32_t _next;
+#if !SAMPLER_DETERMINISTIC
+  MWC rng;
+#endif
+  
+public:
+  Sampler()
+  {
+#if !SAMPLER_DETERMINISTIC
+    _next = rng.geometric(SAMPLE_PROBABILITY);
+#else
+    _next = SAMPLE_RATE;
+#endif
+  }
+  
+  inline int sample(size_t sz) {
+    auto oldNext = _next;
+    _next -= sz;
+    if (unlikely(_next <= 0)) {
+#if SAMPLER_DETERMINISTIC
+      _next = SAMPLE_RATE;
+#else
+      _next = rng.geometric(SAMPLE_PROBABILITY);
+#endif
+      if (sz >= SAMPLE_RATE) {
+	// return ((SAMPLE_RATE - oldNext) + sz) / SAMPLE_RATE;
+	return sz / SAMPLE_RATE + 1;
+      } else {
+	return 1;
+      }
+    }
+    return 0;
+  }
+  
+private:
+  static constexpr double SAMPLE_PROBABILITY = (double) 1.0 / (double) SAMPLE_RATE;
+};
