772fbec4d81c285c007a42d01e07dd7b83fd9a78
emeryberger <emery.berger@gmail.com>
2020-07-21 16:36:46 -0400

    Backed off flto.

--- heaplayers-make.mk
+++ heaplayers-make.mk
@@ -1,11 +1,11 @@
-CPPFLAGS = -std=c++17 -flto -g -fno-builtin-malloc -O3 -DNDEBUG -fvisibility=hidden
-# CPPFLAGS = -std=c++17 -g -O0 -fvisibility=hidden
+# CPPFLAGS = -std=c++17 -flto -g -fno-builtin-malloc -O3 -DNDEBUG -fvisibility=hidden
+CPPFLAGS = -std=c++17 -g -O3 -DNDEBUG -fno-builtin-malloc -fvisibility=hidden
 CXX = clang++
 
 INCLUDES = -I. -I./include -IHeap-Layers -IHeap-Layers/wrappers -IHeap-Layers/utility
 
 MACOS_SRC = lib$(LIBNAME).cpp Heap-Layers/wrappers/macwrapper.cpp
-MACOS_COMPILE = $(CXX) -ftemplate-depth=1024 -arch x86_64 -pipe $(CPPFLAGS) $(INCLUDES) -D_REENTRANT=1 -compatibility_version 1 -current_version 1 -D'CUSTOM_PREFIX(x)=xx\#\#x' $(MACOS_SRC) -dynamiclib -install_name $(DESTDIR)$(PREFIX)/lib$(LIBNAME).dylib -o lib$(LIBNAME).dylib -ldl -lpthread 
+MACOS_COMPILE = $(CXX) -ftls-model=initial-exec -ftemplate-depth=1024 -arch x86_64 -pipe $(CPPFLAGS) $(INCLUDES) -D_REENTRANT=1 -compatibility_version 1 -current_version 1 -D'CUSTOM_PREFIX(x)=xx\#\#x' $(MACOS_SRC) -dynamiclib -install_name $(DESTDIR)$(PREFIX)/lib$(LIBNAME).dylib -o lib$(LIBNAME).dylib -ldl -lpthread 
 
 LINUX_SRC = lib$(LIBNAME).cpp Heap-Layers/wrappers/gnuwrapper.cpp
 LINUX_COMPILE = $(CXX) $(CPPFLAGS) -D'CUSTOM_PREFIX(x)=xx\#\#x' -I/usr/include/nptl -fno-builtin-malloc -pipe -fPIC $(INCLUDES) -D_REENTRANT=1 -shared $(LINUX_SRC) -Bsymbolic -o lib$(LIBNAME).so -ldl -lpthread
