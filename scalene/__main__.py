af282f0dfeef251439bfeacff2f797918f62a027
Kenneth Hoste <kenneth.hoste@ugent.be>
2020-09-23 19:55:53 +0200

    expose errors when calling main

--- scalene/__main__.py
+++ scalene/__main__.py
@@ -1,9 +1,12 @@
+import sys
+
 def main():
     try:
         from scalene import scalene
         scalene.Scalene.main()
-    except:
-        pass
+    except Exception as exc:
+        sys.stderr.write("ERROR: Calling scalene main function failed: %s\n" % exc)
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
