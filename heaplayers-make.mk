2a8985580733e825a8e7c744ec7b817a704c43b3
emeryberger <emery.berger@gmail.com>
2020-10-28 12:52:10 -0400

    Removed spurious extra -fno-builtin-malloc.

--- heaplayers-make.mk
+++ heaplayers-make.mk
@@ -1,3 +1,4 @@
+# CPPFLAGS = -std=c++17 -g -O0 -flto -DNDEBUG -fno-builtin-malloc -fvisibility=hidden
 CPPFLAGS = -std=c++17 -g -O3 -flto -DNDEBUG -fno-builtin-malloc -fvisibility=hidden
 CXX = clang++
 
@@ -7,7 +8,7 @@ MACOS_SRC = lib$(LIBNAME).cpp Heap-Layers/wrappers/macwrapper.cpp
 MACOS_COMPILE = $(CXX) -ftls-model=initial-exec -ftemplate-depth=1024 -arch x86_64 -pipe $(CPPFLAGS) $(INCLUDES) -D_REENTRANT=1 -compatibility_version 1 -current_version 1 -D'CUSTOM_PREFIX(x)=xx\#\#x' $(MACOS_SRC) -dynamiclib -install_name $(DESTDIR)$(PREFIX)/lib$(LIBNAME).dylib -o lib$(LIBNAME).dylib -ldl -lpthread 
 
 LINUX_SRC = lib$(LIBNAME).cpp Heap-Layers/wrappers/gnuwrapper.cpp
-LINUX_COMPILE = $(CXX) $(CPPFLAGS) -D'CUSTOM_PREFIX(x)=xx\#\#x' -I/usr/include/nptl -fno-builtin-malloc -pipe -fPIC $(INCLUDES) -D_REENTRANT=1 -shared $(LINUX_SRC) -Bsymbolic -o lib$(LIBNAME).so -ldl -lpthread
+LINUX_COMPILE = $(CXX) $(CPPFLAGS) -D'CUSTOM_PREFIX(x)=xx\#\#x' -I/usr/include/nptl -pipe -fPIC $(INCLUDES) -D_REENTRANT=1 -shared $(LINUX_SRC) -Bsymbolic -o lib$(LIBNAME).so -ldl -lpthread
 
 UNAME_S := $(shell uname -s)
 UNAME_P := $(shell uname -p)
