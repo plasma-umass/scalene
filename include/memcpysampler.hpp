914101332b9920ad417d6865d586fca035580675
Emery Berger <emery.berger@gmail.com>
2020-12-24 16:41:29 -0500

    Revert "Moved memcpy signal file and counter to mmap, abstracted out writing to mmaped files"

--- include/memcpysampler.hpp
+++ include/memcpysampler.hpp
@@ -11,8 +11,6 @@
 #if defined(__x86_64__)
 #include "rtememcpy.h"
 #endif
-#include "samplefile.hpp"
-
 
 template <uint64_t MemcpySamplingRateBytes>
 class MemcpySampler {
@@ -22,12 +20,20 @@ class MemcpySampler {
   static constexpr auto fname = "/tmp/scalene-memcpy-signalXXXXX";
 public:
   MemcpySampler()
-    : _samplefile((char*) "/tmp/scalene-memcpy-signal@", (char*) "/tmp/scalene-memcpy-lock@"),
-      _interval (MemcpySamplingRateBytes),
+    : _interval (MemcpySamplingRateBytes),
       _memcpyOps (0),
       _memcpyTriggered (0)
   {
     signal(MemcpySignal, SIG_IGN);
+    auto pid = getpid();
+    int i;
+    for (i = 0; i < local_strlen(fname); i++) {
+      if (fname[i] == 'X') {
+	break;
+      }
+      scalene_memcpy_signal_filename[i] = fname[i];
+    }
+    stprintf::stprintf((char *) &scalene_memcpy_signal_filename[i], "@", pid);
   }
 
   int local_strlen(const char * str) {
@@ -37,6 +43,10 @@ public:
       str++;
     }
     return len;
+  }
+  
+  ~MemcpySampler() {
+    unlink(scalene_memcpy_signal_filename);
   }
 
   ATTRIBUTE_ALWAYS_INLINE inline void * memcpy(void * dst, const void * src, size_t n) {
@@ -62,7 +72,6 @@ private:
 
   //// local implementations of memcpy and friends.
   Sampler<MemcpySamplingRateBytes> _memcpySampler;
-  SampleFile _samplefile;
   ATTRIBUTE_ALWAYS_INLINE inline void * local_memcpy(void * dst, const void * src, size_t n) {
 #if defined(__APPLE__)
     return ::memcpy(dst, src, n);
@@ -109,12 +118,14 @@ private:
   uint64_t _memcpyOps;
   unsigned long long _memcpyTriggered;
   uint64_t _interval;
+  char scalene_memcpy_signal_filename[255];
 
   void writeCount() {
     char buf[255];
-
-    stprintf::stprintf(buf, "@,@\n\n", _memcpyTriggered, _memcpyOps);
-    _samplefile.writeToFile(buf);
+    stprintf::stprintf(buf, "@,@\n", _memcpyTriggered, _memcpyOps);
+    int fd = open(scalene_memcpy_signal_filename, flags, perms);
+    write(fd, buf, strlen(buf));
+    close(fd);
   }
 };
 
--- include/sampleheap.hpp
+++ include/sampleheap.hpp
@@ -17,7 +17,6 @@
 #include "common.hpp"
 #include "open_addr_hashtable.hpp"
 #include "sampler.hpp"
-#include "samplefile.hpp"
 #include "stprintf.h"
 #include "tprintf.h"
 
@@ -29,9 +28,8 @@ typedef std::atomic<uint64_t> counterType;
 typedef uint64_t counterType;
 #endif
 
-
 template <uint64_t MallocSamplingRateBytes, class SuperHeap> 
-class SampleHeap : public SuperHeap{
+class SampleHeap : public SuperHeap {
 
   static constexpr int MAX_FILE_SIZE = 4096 * 65536;
   
@@ -42,8 +40,7 @@ public:
   enum { CallStackSamplingRate = MallocSamplingRateBytes * 10 }; // 10 here just to reduce overhead
 
   SampleHeap()
-    : _samplefile((char*) "/tmp/scalene-malloc-signal@", (char*) "/tmp/scalene-malloc-lock@"),
-      _mallocTriggered (0),
+    : _mallocTriggered (0),
       _freeTriggered (0),
       _pythonCount (0),
       _cCount (0),
@@ -52,9 +49,23 @@ public:
     // Ignore these signals until they are replaced by a client.
     signal(MallocSignal, SIG_IGN);
     signal(FreeSignal, SIG_IGN);
-
+    // Set up the log file.
+    auto pid = getpid();
+    stprintf::stprintf(scalene_malloc_signal_filename, "/tmp/scalene-malloc-signal@", pid);
+    _fd = open(scalene_malloc_signal_filename, flags, perms);
+    // Make it so the file can reach the maximum size.
+    ftruncate(_fd, MAX_FILE_SIZE);
+    _mmap = reinterpret_cast<char *>(mmap(0, MAX_FILE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, _fd, 0));
+    if (_mmap == MAP_FAILED) {
+      tprintf::tprintf("Scalene: internal error = @\n", errno);
+      abort();
+    }
   }
 
+  ~SampleHeap() {
+    // Delete the log file.
+    unlink(scalene_malloc_signal_filename);
+  }
   
   ATTRIBUTE_ALWAYS_INLINE inline void * malloc(size_t sz) {
     auto ptr = SuperHeap::malloc(sz);
@@ -131,15 +142,15 @@ private:
   Sampler<MallocSamplingRateBytes> _mallocSampler;
   Sampler<MallocSamplingRateBytes> _freeSampler;
   Sampler<CallStackSamplingRate>   _callStackSampler;
-
-  SampleFile _samplefile;
-
   counterType _mallocTriggered;
   counterType _freeTriggered;
   counterType _pythonCount;
   counterType _cCount;
 
   open_addr_hashtable<65536> _table; // Maps call stack entries to function names.
+  char scalene_malloc_signal_filename[256];
+  int _fd;       // true file descriptor for the log
+  char * _mmap;  // address of the first byte of the log
   int _lastpos;  // last position written into the log
   
   void recordCallStack(size_t sz) {
@@ -258,6 +269,7 @@ private:
   static constexpr auto perms = S_IRUSR | S_IWUSR;
 
   void writeCount(AllocSignal sig, uint64_t count) {
+    const auto MAX_BUFSIZE = 1024;
     char buf[MAX_BUFSIZE];
     if (_pythonCount == 0) {
       _pythonCount = 1; // prevent 0/0
@@ -271,7 +283,7 @@ private:
 		       (float) _pythonCount / (_pythonCount + _cCount));
 #else
     //    tprintf::tprintf("count = @\n", count);
-    snprintf(buf,
+    snprintf(_mmap + _lastpos,
 	     MAX_BUFSIZE,
 #if defined(__APPLE__)
 	     "%c,%llu,%llu,%f\n\n",
@@ -283,11 +295,7 @@ private:
 	     count,
 	     (float) _pythonCount / (_pythonCount + _cCount));
 #endif
-//       tprintf::tprintf("Writing ");
-//       tprintf::tprintf(buf);
-//       tprintf::tprintf("\n");
-     _samplefile.writeToFile(buf);
-    // _lastpos += strlen(_mmap + _lastpos) - 1;
+    _lastpos += strlen(_mmap + _lastpos) - 1;
   }
 
 };
