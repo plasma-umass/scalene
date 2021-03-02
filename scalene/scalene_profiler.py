4c46f4f408d8b9dc4b144cb1912a1700ea906240
Emery Berger <emery.berger@gmail.com>
2021-03-02 17:03:00 -0500

    Change exception to if.

--- scalene/scalene_profiler.py
+++ scalene/scalene_profiler.py
@@ -930,10 +930,8 @@ class Scalene:
                 f = cast(FrameType, frame.f_back)
             fn_name = Filename(f.f_code.co_name)
             # Prepend the class, if any
-            try:
+            if "self" in f.f_locals:
                 fn_name = f.f_locals["self"].__class__.__name__ + "." + fn_name
-            except BaseException:
-                pass
             stats.function_map[fname][lineno] = fn_name
             bytei = ByteCodeIndex(frame.f_lasti)
             # Add the byte index to the set for this line (if it's not there already).
