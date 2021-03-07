bbdc1e44ce6d6a94a6b5fde29a7f273ade1e2884
Emery Berger <emery.berger@gmail.com>
2021-03-07 00:33:32 -0500

    Parse arguments from the '%%scalene' line.

--- scalene/__init__.py
+++ scalene/__init__.py
@@ -5,27 +5,30 @@ try:
     from IPython.core.page import page
     from scalene.scalene_arguments import ScaleneArguments
     import os
+    import sys
     import tempfile
     import textwrap
     
     @magics_class
     class ScaleneMagics(Magics):
-
         @cell_magic
         def scalene(self, line, cell=None) -> None:
-            # TODO: parse arguments from line
-            if cell:
-                from scalene import scalene_profiler
+            from scalene import scalene_profiler
+            if line:
+                sys.argv = ["scalene"]
+                sys.argv.extend(line.split(" "))
+                (args, left) = scalene_profiler.Scalene.parse_args()
+            else:
                 args = ScaleneArguments()
-                args.cpu_only = True # full Scalene is not yet working
+            args.cpu_only = True # full Scalene is not yet working, force to use CPU-only mode
+            if cell:
                 # Create a temporary file to hold the supplied code.
-                code = cell # "def execution_cell():\n" + textwrap.indent(cell, "\t") + "\nexecution_cell()"
+                code = cell
                 tmpfile = tempfile.NamedTemporaryFile(mode="w+", delete=False, prefix="scalene_", suffix=".py")
                 tmpfile.write(code)
                 tmpfile.close()
                 # For now, only profile the temporary file (that is, the current cell).
                 args.profile_only = os.path.basename(tmpfile.name)
-                # profiler = scalene_profiler.Scalene(args, tmpfile.name)
                 scalene_profiler.Scalene.run_profiler(args, [tmpfile.name])
 
     def load_ipython_extension(ip):
