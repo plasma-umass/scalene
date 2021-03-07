c2a61bb842e1037fc53f7ad5a7ca2e2f41fa0121
Emery Berger <emery.berger@gmail.com>
2021-03-06 20:37:16 -0500

    Made more object-oriented.

--- scalene/scalene_arguments.py
+++ scalene/scalene_arguments.py
@@ -1,18 +1,20 @@
 import argparse
 
 class ScaleneArguments(argparse.Namespace):
-    cpu_only = False
-    cpu_percent_threshold = 1
-    cpu_sampling_rate = 0.01
-    html = False
-    malloc_threshold = 100
-    outfile = None
-    pid = 0
-    profile_all = True
-    profile_interval = float("inf")
-    profile_only = ""
-    reduced_profile = False
-    use_virtual_time = False
+
+    def __init__(self):
+        self.cpu_only = False
+        self.cpu_percent_threshold = 1
+        self.cpu_sampling_rate = 0.01
+        self.html = False
+        self.malloc_threshold = 100
+        self.outfile = None
+        self.pid = 0
+        self.profile_all = False
+        self.profile_interval = float("inf")
+        self.profile_only = ""
+        self.reduced_profile = False
+        self.use_virtual_time = False
     
     
     
