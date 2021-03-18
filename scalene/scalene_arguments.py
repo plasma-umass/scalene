da8034da768160ab24d5e39ac672cf76664b049a
Emery Berger <emery.berger@gmail.com>
2021-03-18 14:03:39 -0400

    Added some comments.

--- scalene/scalene_arguments.py
+++ scalene/scalene_arguments.py
@@ -5,15 +5,21 @@ class ScaleneArguments(argparse.Namespace):
     def __init__(self) -> None:
         self.cpu_only = False
         self.cpu_percent_threshold = 1
+        # mean seconds between interrupts for CPU sampling.
         self.cpu_sampling_rate = 0.01
         self.html = False
         self.malloc_threshold = 100
         self.outfile = None
         self.pid = 0
+        # if we profile all code or just target code and code in its child directories
         self.profile_all = False
+        # how long between outputting stats during execution
         self.profile_interval = float("inf")
+        # what function pathnames must contain to be output during profiling
         self.profile_only = ""
+        # reduced profile?
         self.reduced_profile = False
+        # do we use virtual time or wallclock time (capturing system time and blocking)?
         self.use_virtual_time = False
     
     
