c8c50beb1949d5af186744c111fa82d8b54dd308
Emery Berger <emery.berger@gmail.com>
2021-03-31 14:47:30 -0400

    Fixed.

--- scalene/scalene_gpu.py
+++ scalene/scalene_gpu.py
@@ -1,5 +1,6 @@
 import pynvml
 
+
 class scalene_gpu:
     """A wrapper around the nvidia device driver library (nvidia-ml-py)."""
 
@@ -11,7 +12,7 @@ class scalene_gpu:
             pynvml.nvmlInit()
             self.__has_gpu = True
             self.__ngpus = pynvml.nvmlDeviceGetCount()
-            for i in range(self.ngpus):
+            for i in range(self.__ngpus):
                 self.__handle.append(pynvml.nvmlDeviceGetHandleByIndex(i))
         except:
             pass
@@ -19,17 +20,17 @@ class scalene_gpu:
     def has_gpu(self):
         return self.__has_gpu
 
-    def gpu_load(self):
+    def load(self):
         if self.__has_gpu:
-            load = 0.0
+            l = 0.0
             for i in range(self.__ngpus):
-                load += pynvml.nvmlDeviceGetUtilizationRates(handle).gpu
-            return load / self.__ngpus
+                l += pynvml.nvmlDeviceGetUtilizationRates(self.__handle[i]).gpu
+            return l / self.__ngpus
         return 0.0
 
     def memory_used(self):
         mem_used = 0
         for i in range(self.__ngpus):
-            mem_info = pynvml.nvmlDeviceGetMemoryInfo(handle[i])
+            mem_info = pynvml.nvmlDeviceGetMemoryInfo(self.__handle[i])
             mem_used += mem_info.used
         return mem_used
