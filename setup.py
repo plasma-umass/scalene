8c54f99a63847188d55c7f70312eb17fbd02694d
Emery Berger <emery.berger@gmail.com>
2021-05-22 19:58:53 -0400

    Added nmake for Windows builds.

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 try:
-    cmd = "make"
+    if sys.platform == 'win32':
+      cmd = "nmake"
+    else:
+      cmd = "make"
     out = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
 except Exception as e:
     if isinstance(e, subprocess.CalledProcessError):
