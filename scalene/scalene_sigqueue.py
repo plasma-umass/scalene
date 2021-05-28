2f563eafed0e5fc19393477b64e405e15d728f41
Juan Altmayer Pizzorno <juan@altmayer.com>
2021-05-28 10:53:26 -0400

    - added rationale for thread handling, per Emery's feedback;

--- scalene/scalene_sigqueue.py
+++ scalene/scalene_sigqueue.py
@@ -16,12 +16,15 @@ class ScaleneSigQueue:
 
     def start(self):
         assert not self.thread
+        # We use a daemon thread to defensively avoid hanging if we never join with it
         self.thread = threading.Thread(target=self.run, daemon=True)
         self.thread.start()
 
     def stop(self):
         if self.thread:
             self.queue.put(None)
+            # We need to join all threads before a fork() to avoid an inconsistent
+            # state, locked mutexes, etc.
             self.thread.join()
             self.thread = None
 
