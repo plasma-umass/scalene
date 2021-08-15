ef16863c1d0049ca65b154bbe9cf2674cc6ab423
Emery Berger <emery.berger@gmail.com>
2021-08-15 19:22:16 -0400

    Use pymalloc (which necessitates tracking mmap/munmap for arenas but is MUCH faster).

--- scalene/scalene_preload.py
+++ scalene/scalene_preload.py
@@ -27,7 +27,8 @@ class ScalenePreload:
                 env["DYLD_INSERT_LIBRARIES"] = os.path.join(
                     scalene.__path__[0], "libscalene.dylib"
                 )
-                env["PYTHONMALLOC"] = "malloc"
+                # env["PYTHONMALLOC"] = "malloc"
+                env["PYTHONMALLOC"] = "pymalloc"
             # required for multiprocessing support, even without libscalene
             env["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
 
