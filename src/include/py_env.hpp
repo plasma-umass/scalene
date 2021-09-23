f80ecd865116547500b828d280e84f8018ae952e
Juan Altmayer Pizzorno <juan@altmayer.com>
2021-09-23 16:19:41 -0400

    - trying to please the C++ compiler on the anylinux python container;

--- src/include/py_env.hpp
+++ src/include/py_env.hpp
@@ -79,6 +79,6 @@ static PyStringPtrList py_string_ptr_list;
 
 static void set_py_string_ptr_list(PyObject* p, PyObject* base_path,
                                    bool trace_all) {
-  std::lock_guard g(_mx);
+  std::lock_guard<decltype(_mx)> g(_mx);
   py_string_ptr_list = PyStringPtrList{p, base_path, trace_all};
 }
