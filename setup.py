7379e605fff97fb2e7d2e9db4c5a4357818b2b7f
Juan Altmayer Pizzorno <juan@altmayer.com>
2021-09-27 19:33:28 -0400

    - continuing;

--- setup.py
+++ setup.py
@@ -93,9 +93,10 @@ testing = 'TWINE_REPOSITORY' in environ and environ['TWINE_REPOSITORY'] == 'test
 if testing:
     import subprocess
     import time
-    version_timestamp = int(subprocess.check_output(["git", "log", "-1", "--format=%ct",
-                                                     "scalene/scalene_version.py"]))
-    mins_since_version = (time.time() - version_timestamp)/60
+    version_timestamp = subprocess.check_output(["git", "log", "-1", "--format=%ct",
+                                                 "scalene/scalene_version.py"])
+    print(version_timestamp)
+    mins_since_version = (time.time() - int(version_timestamp))/60
 
 setup(
     name="scalene",
