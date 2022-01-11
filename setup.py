e8683cae816e3931adfbd868bf557ec625a705f9
Emery Berger <emery.berger@gmail.com>
2022-01-10 20:40:31 -0500

    We support 3.10.

--- setup.py
+++ setup.py
@@ -137,6 +137,7 @@ setup(
     [
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS :: MacOS X",
