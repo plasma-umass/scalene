d13c3dc88e04e9261cc1f64ea90792d129323e0d
Emery Berger <emery.berger@gmail.com>
2022-01-12 12:44:56 -0500

    Removed unnecessary computations, now at 16000x faster, H/T mikeblas.

--- test/test-martinheinz.py
+++ test/test-martinheinz.py
@@ -19,6 +19,7 @@ start = time.time()
 
 print("Original:")
 
+
 d1_orig = exp(Decimal(150))
 d2_orig = exp(Decimal(400))
 d3_orig = exp(Decimal(3000))
@@ -34,8 +35,8 @@ def exp_opt(x):
     while s != lasts:
         lasts = s
         i += 1
-        fact *= i
-        num *= x
+        # was: fact *= i
+        # was: num *= x
         nf *= (x / i) ### update nf to be num / fact
         s += nf ### was: s += num / fact
     getcontext().prec -= 2
@@ -58,4 +59,6 @@ print("Improvement: ", elapsed_original / elapsed_optimized)
 assert d1_orig == d1_opt
 assert d2_orig == d2_opt
 assert d3_orig == d3_opt
+
+print("All equivalent? ", d1_orig == d1_opt and d2_orig == d2_opt and d3_orig == d3_opt)
 
