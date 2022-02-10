fac038912c49f512d664bfffdfe675b4fd2c1cf9
Sam Stern <sam@samstern.me>
2022-02-10 14:52:48 -0500

    Updated tests

--- test/test_timers.py
+++ test/test_timers.py
@@ -1,21 +1,22 @@
-from curses import nonl
 import signal
 import time
 
 
 start = -1
-loop = True
+loop = 10
 def callback(*args):
     global loop
+    global start
     print(time.perf_counter() - start)
-    loop = False
+    start = time.perf_counter()
+    loop -= 1
 
 signal.signal(signal.SIGALRM, callback)
 
 start = time.perf_counter()
-signal.setitimer(signal.ITIMER_REAL, 5)
+signal.setitimer(signal.ITIMER_REAL, 5, 1)
 
 i = 0
-while loop:
+while loop > 0:
     i += 1
     time.sleep(0.1)
