1b78acfb6f86e33542345e58c584a710595e862b
sternj <sam@samstern.me>
2022-02-12 20:54:03 -0500

    Removed some "ignore"s

--- scalene/scalene_profiler.py
+++ scalene/scalene_profiler.py
@@ -393,7 +393,8 @@ class Scalene:
     ) -> None:
         invalidated = Scalene.__last_profiled_invalidated
         (fname, lineno, lasti) = Scalene.__last_profiled
-        Scalene.enter_function_meta(this_frame, Scalene.__stats) # type: ignore
+        if this_frame:
+            Scalene.enter_function_meta(this_frame, Scalene.__stats)
         # Walk the stack till we find a line of code in a file we are tracing.
         found_frame = False
         f = this_frame
@@ -434,7 +435,8 @@ class Scalene:
         ],
         this_frame: Optional[FrameType],
     ) -> None:
-        Scalene.enter_function_meta(this_frame, Scalene.__stats) # type: ignore
+        if this_frame:
+            Scalene.enter_function_meta(this_frame, Scalene.__stats)
         Scalene.__alloc_sigq.put([0])
         del this_frame
 
