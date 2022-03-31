3a64fd8cebd5f7e85457f629171f28d7fbf3372b
Sam Stern <sam@samstern.me>
2022-03-31 19:14:43 -0400

    Updated pool test somewhat

--- test/pool-test.py
+++ test/pool-test.py
@@ -1,3 +1,15 @@
+# import logging
 import multiprocessing
-pool = multiprocessing.Pool(processes=1)
-pool.terminate()
+# log = multiprocessing.get_logger()
+# log.setLevel(logging.DEBUG)
+# log.addHandler(logging.StreamHandler())
+from multiprocessing import Pool
+
+def f(x):
+    print("Start")
+    return [i for i in range(1000000)]
+
+if __name__ == '__main__':
+    with Pool(5) as p:
+        q = p.map(f, [1, 2, 3])
+        print(len(q))
