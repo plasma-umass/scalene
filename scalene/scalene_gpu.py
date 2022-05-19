bc7f2117cb79b5146bd4792eacc2bc9c142f604e
Emery Berger <emery.berger@gmail.com>
2022-05-19 12:27:11 -0400

    Added per-process GPU utilization accounting, when available.

--- scalene/scalene_gpu.py
+++ scalene/scalene_gpu.py
@@ -13,12 +13,55 @@ class ScaleneGPU:
         self.__has_gpu = False
         self.__handle = []
         self.__pid = os.getpid()
-        with contextlib.suppress(Exception):
-            pynvml.nvmlInit()
-            self.__has_gpu = True
-            self.__ngpus = pynvml.nvmlDeviceGetCount()
-            for i in range(self.__ngpus):
-                self.__handle.append(pynvml.nvmlDeviceGetHandleByIndex(i))
+        # with contextlib.suppress(Exception):
+        pynvml.nvmlInit()
+        self.__has_gpu = True
+        self.__ngpus = pynvml.nvmlDeviceGetCount()
+        for i in range(self.__ngpus):
+            handle = pynvml.nvmlDeviceGetHandleByIndex(i)
+            self.__handle.append(handle)
+        self.__has_per_pid_accounting = self.set_accounting_mode()
+
+    def __del__(self) -> None:
+        if not self.__has_per_pid_accounting:
+            print("NOTE: The GPU is currently running in a mode that reduces Scalene's accuracy when reporting GPU utilization.")
+            print("Run once as root (i.e., prefixed with `sudo`) to enable per-process GPU accounting.")
+        
+    def set_accounting_mode(self) -> bool:
+        """Returns true iff the accounting mode was set already for all GPUs or is now set."""
+        ngpus = pynvml.nvmlDeviceGetCount()
+
+        for i in range(ngpus):
+            # Check if each GPU has accounting mode set.
+            h = pynvml.nvmlDeviceGetHandleByIndex(i)
+            if pynvml.nvmlDeviceGetAccountingMode(h) != pynvml.NVML_FEATURE_ENABLED:
+                # If not, try to set it.
+                try:
+                    pynvml.nvmlDeviceSetAccountingMode(h, pynvml.NVML_FEATURE_ENABLED)
+                except pynvml.NVMLError:
+                    # We don't have sufficient permissions.
+                    return False
+                    pass
+
+        return True
+
+    def gpu_utilization(self, pid) -> float:
+        """Return overall GPU utilization by pid if possible.
+
+        Otherwise, returns aggregate utilization across all running processes."""
+        ngpus = pynvml.nvmlDeviceGetCount()
+        accounting_on = self.__has_per_pid_accounting
+        utilization = 0
+        for i in range(ngpus):
+            h = pynvml.nvmlDeviceGetHandleByIndex(i)
+            if accounting_on:
+                try:
+                    utilization += pynvml.nvmlDeviceGetAccountingStats(h, pid).gpuUtilization
+                except:
+                    pass
+            else:
+                utilization += pynvml.nvmlDeviceGetUtilizationRates(h).gpu
+        return (utilization / ngpus) / 100.0
 
     def has_gpu(self) -> bool:
         """True iff the system has a detected GPU."""
@@ -47,17 +90,7 @@ class ScaleneGPU:
     def get_stats(self) -> Tuple[float, float]:
         """Returns a tuple of (utilization %, memory in use)."""
         if self.__has_gpu:
-            total_load = 0.0
-            mem_used = 0
-            for i in range(self.__ngpus):
-                with contextlib.suppress(Exception):
-                    total_load += pynvml.nvmlDeviceGetUtilizationRates(
-                        self.__handle[i]
-                    ).gpu
-                    #mem_info = pynvml.nvmlDeviceGetMemoryInfo(self.__handle[i])
-                    #mem_used += mem_info.used
-            total_load = (total_load / self.__ngpus) / 100.0
+            total_load = self.gpu_utilization(self.__pid)
             mem_used = self.gpu_memory_usage(self.__pid)
-            print (total_load, mem_used)
             return (total_load, mem_used)
         return (0.0, 0.0)
