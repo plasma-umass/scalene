8c5bb74f454c4f850f82b4b1ada984364e47618f
Emery Berger <emery.berger@gmail.com>
2022-11-04 11:00:57 -0400

    Removed gitpython dependency.

--- setup.py
+++ setup.py
@@ -189,7 +189,6 @@ setup(
         "rich>=9.2.0",
         "cloudpickle>=1.5.0",
         "pynvml>=11.0.0",
-        "gitpython>=3.1.27",
     ],
     ext_modules=([get_line_atomic, pywhere] if sys.platform != 'win32' else []),
     setup_requires=['setuptools_scm'],
