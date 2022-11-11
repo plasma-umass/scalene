4c0c2309306329a6e4ffb734a34d019e6350559b
Emery Berger <emery.berger@gmail.com>
2022-11-11 17:31:01 -0500

    Changed to use tracking of sampled objects.

--- src/include/poissonsampleinterval.hpp
+++ src/include/poissonsampleinterval.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include <_types/_uint64_t.h>
 #include <random>
+#include <unordered_map>
 
 /**
- * @brief "triggers" samples periodically when |increments-decrements| >
- * SAMPLE_INTERVAL
+ * @brief "triggers" samples using a geometric distribution
+ *
+ * Sampled recording allocated objects with a rate of every
+ * SAMPLE_INTERVAL bytes (on average).
  *
  */
 
@@ -16,27 +19,25 @@ class PoissonSampleInterval {
    */
   PoissonSampleInterval(uint64_t SAMPLE_INTERVAL) : gen(rd()), d(1.0 / SAMPLE_INTERVAL) {
     resetAlloc();
-    resetFree();
   }
 
   /**
-   * @brief decrement by the sample amount, triggering an interval reset when we
-   * cross the threshold
+   * @brief Deallocate an object; if sampled, return the size of the recorded sampling interval, else 0.
    *
-   * @param sample the amount to decrement the sample interval by
+   * @param sample 
    * @return uint64_t the previous sample interval if we crossed it; 0 otherwise
    */
-  inline uint64_t decrement(uint64_t sample) {
-    return increment(sample);
-    
-    if (unlikely(sample > _tillNextFree)) {
-      auto prev = _countdownFree;
-      auto diff = sample - _tillNextFree;
-      resetFree();
-      return prev + diff;
+  inline uint64_t decrement(uint64_t, void * ptr = nullptr) {
+    auto found = _allocSize.find(ptr) != _allocSize.end();
+    if (!found) {
+      // Not found
+      return 0;
+    } else {
+      // It was sampled. Return the recorded size, removing the object first.
+      auto sz = _allocSize[ptr];
+      _allocSize.erase(ptr);
+      return sz;
     }
-    _tillNextFree -= sample;
-    return 0;
   }
 
   /**
@@ -46,12 +47,14 @@ class PoissonSampleInterval {
    * @param sample the amount to decrement the sample interval by
    * @return uint64_t the previous sample interval if we crossed it; 0 otherwise
    */
-  inline uint64_t increment(uint64_t sample) {
+  inline uint64_t increment(uint64_t sample, void * ptr = nullptr) {
     if (unlikely(sample > _tillNextAlloc)) {
       auto prev = _countdownAlloc;
       auto diff = sample - _tillNextAlloc;
       resetAlloc();
-      return prev + diff;
+      const auto incrementAmount = prev + diff;
+      _allocSize[ptr] = incrementAmount;
+      return incrementAmount;
     }
     _tillNextAlloc -= sample;
     return 0;
@@ -61,24 +64,16 @@ class PoissonSampleInterval {
 
   std::random_device rd;
   std::mt19937 gen;
-  std::exponential_distribution<float> d;
+  std::geometric_distribution<uint64_t> d;
 
-  uint64_t _tillNextFree;
-  uint64_t _countdownFree;  /// the current countdown to the next sample interval
   uint64_t _tillNextAlloc;
   uint64_t _countdownAlloc;  /// the number of frees since the last sample interval
 
-  void resetFree() {
-    // Generate a new sample from the exponential distribution.
-    _countdownFree = d(gen);
-    _tillNextFree = _countdownFree;
-    printf_("RESET FREE %lu\n", _countdownFree);
-  }
+  std::unordered_map<void *, uint64_t> _allocSize;
   
   void resetAlloc() {
     // Generate a new sample from the exponential distribution.
     _countdownAlloc = d(gen);
     _tillNextAlloc = _countdownAlloc;
-    printf_("RESET ALLOC %lu\n", _countdownAlloc);
   }
 };
