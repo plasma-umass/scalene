bf601c1022c5da25e79d27d07df159be89c9cfc0
Emery Berger <emery.berger@gmail.com>
2023-01-02 09:00:40 -0500

    Lower-case Cython to enable builds on case-sensitive filesystems.

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ setup(
         "Jinja2>=3.0.3",
     ],
     ext_modules=([get_line_atomic, pywhere, crdp] if sys.platform != 'win32' else []),
-    setup_requires=['wheel', 'Cython', 'setuptools_scm'],
+    setup_requires=['wheel', 'cython', 'setuptools_scm'],
     include_package_data=True,
     entry_points={"console_scripts": ["scalene = scalene.__main__:main"]},
     python_requires=">=3.8",
