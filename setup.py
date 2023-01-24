b5e6397d45e4d41aa4469d7436b93e7606c2c865
Sam Stern <sam@samstern.me>
2023-01-23 21:00:32 -0500

    Updated setup.py to exclude 3.10.0

--- setup.py
+++ setup.py
@@ -208,5 +208,5 @@ setup(
     setup_requires=['wheel', 'cython', 'setuptools_scm'],
     include_package_data=True,
     entry_points={"console_scripts": ["scalene = scalene.__main__:main"]},
-    python_requires=">=3.8",
+    python_requires=">=3.8,!=3.10.0",
 )
