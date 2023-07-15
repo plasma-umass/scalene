0165c4b5c871c8eb3c77248c18cce01cad0c4a03
Emery Berger <emery.berger@gmail.com>
2023-07-15 13:46:14 -0400

    Fixes https://github.com/plasma-umass/scalene/issues/625, which is due to a bug caused by NVIDIA (https://github.com/NVIDIA/cuda-python/issues/29).

--- scalene/__init__.py
+++ scalene/__init__.py
@@ -1,3 +1,9 @@
+# Work around this bug: https://github.com/NVIDIA/cuda-python/issues/29
+import os
+os.environ["LC_ALL"] = "POSIX"
+
+print("scalene __init__. OS ENVIRON NOW", os.environ["LC_ALL"])
+
 # Jupyter support
 
 from scalene.scalene_magics import *
