09dfa6ce8ffa56f44769679bfb4963e2041f59ee
Emery Berger <emery.berger@gmail.com>
2023-12-09 20:39:02 -0500

    Fixed Windows browser name.

--- scalene/find_browser.py
+++ scalene/find_browser.py
@@ -4,7 +4,7 @@ from typing import Optional
 def find_browser() -> Optional[webbrowser.BaseBrowser]:
     """Find the default browser if possible and if compatible."""
     # Names of known graphical browsers as per Python's webbrowser documentation
-    graphical_browsers = ["windows-default", "macosx", "safari", "google-chrome", 
+    graphical_browsers = ["windowsdefault", "macosx", "safari", "google-chrome", 
                           "chrome", "chromium", "firefox", "opera", "edge", "mozilla", "netscape"]
     try:
         # Get the default browser object
