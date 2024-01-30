362d9285b8ef4d575d784eebf5e67a5325424327
Emery Berger <emery.berger@gmail.com>
2024-01-30 12:58:41 -0500

    Add profile upload to main screen (#767)
    * Added profile uploading (navbar at bottom, with icon).
    
    * Increased timeouts and heartbeat intervals.

--- scalene/launchbrowser.py
+++ scalene/launchbrowser.py
@@ -63,7 +63,7 @@ class CustomHandler(http.server.SimpleHTTPRequestHandler):
 def monitor_heartbeat():
     global server_running
     while server_running:
-        if time.time() - last_heartbeat > 5:  # 5 seconds timeout
+        if time.time() - last_heartbeat > 30:  # 30 seconds timeout
             print("No heartbeat received, shutting down server...")
             server_running = False
             os._exit(0)
