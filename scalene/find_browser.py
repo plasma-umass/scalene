e8a0abe7c5af05d810b668a345b40ca17dbd87e2
sarahec <sarahec@nextquestion.net>
2024-02-19 11:07:24 -0800

    Exclude text-only browsers

--- scalene/find_browser.py
+++ scalene/find_browser.py
@@ -1,13 +1,18 @@
 import webbrowser
 from typing import Optional
 
-
 def find_browser(browserClass=None) -> Optional[str]:
-    """Find the default system browser"""
+    """Find the default system browser, excluding text browsers.
+    
+    If you want a specific browser, pass its class as an argument."""
+    text_browsers = [
+        "browsh", "elinks", "lynx", "w3m",
+    ]
+
     try:
         # Get the default browser object
         browser = webbrowser.get(browserClass)
-        return browser.name
+        return browser.name if browser.name not in text_browsers else None
     except webbrowser.Error:
         # Return None if there is an error in getting the browser
         return None
