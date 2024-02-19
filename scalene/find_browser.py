7cffc1909ccb71654de192e03d118971505d8194
sarahec <sarahec@nextquestion.net>
2024-02-18 16:33:44 -0800

    Default to the system browser only

--- scalene/find_browser.py
+++ scalene/find_browser.py
@@ -2,34 +2,12 @@ import webbrowser
 from typing import Optional
 
 
-def find_browser() -> Optional[webbrowser.BaseBrowser]:
-    """Find the default browser if possible and if compatible."""
-    # Names of known graphical browsers as per Python's webbrowser documentation
-    graphical_browsers = [
-        "windowsdefault",
-        "macosx",
-        "safari",
-        "google-chrome",
-        "chrome",
-        "chromium",
-        "firefox",
-        "opera",
-        "edge",
-        "mozilla",
-        "netscape",
-    ]
+def find_browser(browserClass=None) -> Optional[str]:
+    """Find the default system browser"""
     try:
         # Get the default browser object
-        browser = webbrowser.get()
-        # Check if the browser's class name matches any of the known graphical browsers
-        browser_class_name = str(type(browser)).lower()
-        if any(
-            graphical_browser in browser_class_name
-            for graphical_browser in graphical_browsers
-        ):
-            return browser
-        else:
-            return None
+        browser = webbrowser.get(browserClass)
+        return browser.name
     except webbrowser.Error:
         # Return None if there is an error in getting the browser
         return None
