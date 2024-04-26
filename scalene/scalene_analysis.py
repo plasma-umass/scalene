eb766a2cc96a802964bc30bd4c4ed80b640e51b8
sternj <sternj@brandeis.edu>
2024-04-26 13:16:51 -0400

    Added newline handling to get_imported_modules

--- scalene/scalene_analysis.py
+++ scalene/scalene_analysis.py
@@ -64,7 +64,7 @@ class ScaleneAnalysis:
             if isinstance(node, (ast.Import, ast.ImportFrom)):
                 imported_modules.append(ast.unparse(node))
 
-        return imported_modules
+        return list(map(str.strip, imported_modules))
 
     @staticmethod
     def get_native_imported_modules(source: str) -> List[str]:
