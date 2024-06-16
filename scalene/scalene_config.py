7577c826daa42d0c736c4ec472c639baefabbf03
Emery Berger <emery.berger@gmail.com>
2024-06-16 15:26:53 -0400

    Bumped version.

--- scalene/scalene_config.py
+++ scalene/scalene_config.py
@@ -1,7 +1,7 @@
 """Current version of Scalene; reported by --version."""
 
-scalene_version = "1.5.41"
-scalene_date = "2024.05.03"
+scalene_version = "1.5.42"
+scalene_date = "2024.06.16"
 
 # Port to use for Scalene UI
 SCALENE_PORT = 11235
