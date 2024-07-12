faa99a0150d1aa5d579710af1fcc05e3598aa48d
emeryberger <emery.berger@gmail.com>
2024-07-12 23:05:17 +0000

    Start on init.

--- scalene/scalene_neuron.py
+++ scalene/scalene_neuron.py
@@ -5,17 +5,22 @@ import time
 import tempfile
 import os
 
-class NeuronMonitor:
-    def __init__(self):
+from typing import Tuple
+
+class ScaleneNeuron:
+    
+    def __init__(self) -> None:
         self._process = None
         self._thread = None
         self._stop_event = threading.Event()
+        self._has_neuron = False
         self._config_path = self._generate_config()
         self.cpu_utilization = 0.0
         self.memory_used_bytes = 0.0
         self.neuroncore_utilization = 0.0
+        self.start()
 
-    def _generate_config(self):
+    def _generate_config(self) -> None:
         config = {
             "period": "1s",
             "system_metrics": [
@@ -48,28 +53,32 @@ class NeuronMonitor:
             json.dump(config, file)
         return temp_file.name
 
-    def start(self):
+    def start(self) -> None:
         self._stop_event.clear()
         self._thread = threading.Thread(target=self._run_monitor)
         self._thread.start()
 
-    def _run_monitor(self):
+    def has_neuron(self) -> bool:
+        return self._has_neuron
+
+    def _run_monitor(self) -> None:
         try:
             self._process = subprocess.Popen(
                 ['neuron-monitor', '-c', self._config_path],
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
             )
+            self._has_neuron = True
             while not self._stop_event.is_set():
                 output = self._process.stdout.readline().strip()
                 if output:
                     self._parse_and_print_output(output)
-                time.sleep(1)
+                # time.sleep(1)
         except Exception as e:
             print(f"Error running neuron-monitor: {e}")
         finally:
             self._process.terminate()
 
-    def _parse_and_print_output(self, output):
+    def _parse_and_print_output(self, output: str) -> None:
         try:
             data = json.loads(output)
             system_data = data.get('system_data', {})
@@ -86,8 +95,9 @@ class NeuronMonitor:
                 if total_cores > 0:
                     average_idle = total_idle / total_cores
                     self.cpu_utilization = 100 - average_idle
-                    
-            #if memory_info:
+
+            # Disabled for now: host memory consumption
+            # if memory_info:
             #    self.memory_used_bytes = memory_info.get('memory_used_bytes', 0)
 
             self.neuroncore_utilization = 0.0
@@ -118,10 +128,11 @@ class NeuronMonitor:
                 total_memory_used = 0
                 for per_core_info in neuron_runtime_data:
                     report = per_core_info.get('report', {})
-                    memory_info = report.get('memory_used', {})
-                    memory_info = memory_info.get('neuron_runtime_used_bytes', {})
-                    memory_info = memory_info.get('usage_breakdown', {})
-                    memory_info = memory_info.get('neuroncore_memory_usage', {})
+                    memory_info = (report
+                                   .get('memory_used', {})
+                                   .get('neuron_runtime_used_bytes', {})
+                                   .get('usage_breakdown', {})
+                                   .get('neuroncore_memory_usage', {}))
                     for core, mem_info in memory_info.items():
                         total_memory_used += sum(mem_info.values())
                         
@@ -132,7 +143,7 @@ class NeuronMonitor:
         except Exception as e:
             print(f"Unexpected error: {e}")
 
-    def stop(self):
+    def stop(self) -> None:
         self._stop_event.set()
         if self._thread:
             self._thread.join()
@@ -141,12 +152,11 @@ class NeuronMonitor:
             self._process.wait()
         os.remove(self._config_path)  # Clean up the temporary file
 
-    def get_stats(self):
+    def get_stats(self) -> Tuple[float, int, float]:
         return self.cpu_utilization, self.memory_used_bytes, self.neuroncore_utilization
 
 if __name__ == "__main__":
-    monitor = NeuronMonitor()
-    monitor.start()
+    monitor = ScaleneNeuron()
     try:
         while True:
             print(monitor.get_stats())
