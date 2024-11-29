721235aa3f8d924f7ad40e40938c9376cb72969b
sternj <sternj@brandeis.edu>
2024-11-29 12:37:39 -0500

    Ctry removing additional check

--- scalene/scalene_profiler.py
+++ scalene/scalene_profiler.py
@@ -1289,9 +1289,9 @@ class Scalene:
                 bytei,
             ) = item
             is_malloc = action == Scalene.MALLOC_ACTION
-            if count == scalene.scalene_config.NEWLINE_TRIGGER_LENGTH + 1: 
-                continue # in previous implementations, we were adding NEWLINE to the footprint.
-                         # We should not account for this in the user-facing profile. 
+            # if count == scalene.scalene_config.NEWLINE_TRIGGER_LENGTH + 1: 
+            #     continue # in previous implementations, we were adding NEWLINE to the footprint.
+            #              # We should not account for this in the user-facing profile. 
             count /= Scalene.BYTES_PER_MB
             if is_malloc:
                 stats.current_footprint += count
