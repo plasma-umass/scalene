96c753a0763887558e35528b9a279955e39b11ce
Emery Berger <emery.berger@gmail.com>
2025-01-12 10:19:42 -0600

    Force UTF-8 generation.

--- scalene/scalene-gui/webpack.config.js
+++ scalene/scalene-gui/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
   entry: './scalene-gui.js',
@@ -11,30 +12,43 @@ module.exports = {
   },
   resolve: {
     fallback: {
-      "crypto": require.resolve("crypto-browserify"),
-      "stream": require.resolve("stream-browserify"),
-      "buffer": require.resolve("buffer"),
-      "util": require.resolve("util"),
-      "assert": require.resolve("assert"),
-      "os": require.resolve("os-browserify/browser"),
-      "http": require.resolve("stream-http"),
-      "https": require.resolve("https-browserify"),
-      "url": require.resolve("url/"),
-      "zlib": require.resolve("browserify-zlib"),
-      "path": require.resolve("path-browserify"),
-      "fs": false,
+      crypto: require.resolve('crypto-browserify'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer'),
+      util: require.resolve('util'),
+      assert: require.resolve('assert'),
+      os: require.resolve('os-browserify/browser'),
+      http: require.resolve('stream-http'),
+      https: require.resolve('https-browserify'),
+      url: require.resolve('url/'),
+      zlib: require.resolve('browserify-zlib'),
+      path: require.resolve('path-browserify'),
+      fs: false,
     },
   },
   plugins: [
     new webpack.ProvidePlugin({
       process: 'process/browser',
     }),
+    new webpack.DefinePlugin({
+      'process.env.LANG': JSON.stringify('en_US.UTF-8'),
+    }),
   ],
   optimization: {
-    minimize: true,            // Enable minimization
-    usedExports: false,         // Disable tree shaking
-    sideEffects: false,         // Include all files, assuming they have side effects
-    concatenateModules: false,  // Disable module concatenation (scope hoisting)
-    innerGraph: false,          // Disable inner graph analysis
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          output: {
+            ascii_only: true, // Escape non-ASCII characters
+          },
+        },
+      }),
+    ],
+    usedExports: false,
+    sideEffects: false,
+    concatenateModules: false,
+    innerGraph: false,
   },
+  devtool: 'source-map', // Enable debugging via source maps
 };
