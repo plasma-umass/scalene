c960f18c697186ba45c46b7beb210a51e3a50faf
Emery Berger <emery.berger@gmail.com>
2025-05-05 12:31:37 -0400

    Handle latest versions of IPython. Fixes https://github.com/plasma-umass/scalene/issues/915.

--- scalene/scalene_jupyter.py
+++ scalene/scalene_jupyter.py
@@ -32,7 +32,10 @@ class ScaleneJupyter:
         # Display the profile in a cell. Starts a webserver to host the iframe holding the profile.html file,
         # which lets JavaScript run (can't do this with `display`, which strips out JavaScript), and then
         # tears down the server.
-        from IPython.core.display import display
+        try:
+            from IPython.core.display import display
+        except:
+            from IPython.display import display
         from IPython.display import IFrame
 
         class RequestHandler(BaseHTTPRequestHandler):
