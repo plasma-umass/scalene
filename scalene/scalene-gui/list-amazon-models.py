8c069777b0d13ccc98d63f77522709533ae3a6b5
Emery Berger <emery.berger@gmail.com>
2025-08-09 14:55:38 -0400

    Utility to list Bedrock models.

---
+++ scalene/scalene-gui/list-amazon-models.py
@@ -0,0 +1,7 @@
+import boto3
+
+client = boto3.client('bedrock')
+response = client.list_foundation_models()
+for model_summary in response['modelSummaries']:
+    print(f"Model ID: {model_summary['modelId']}, Provider: {model_summary['providerName']}")
+    
