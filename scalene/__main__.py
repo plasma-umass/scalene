6c87af0acb546a63b25c001a041350806b49c066
Emery Berger <emery.berger@gmail.com>
2025-09-01 15:44:56 -0400

    Cleanup; fixes https://github.com/plasma-umass/scalene/issues/947.

--- scalene/__main__.py
+++ scalene/__main__.py
@@ -1,15 +1,6 @@
 import sys
 import traceback
 
-from scalene import scalene_profiler
-
-
-def should_trace(s: str) -> bool:
-    if scalene_profiler.Scalene.is_done():
-        return False
-    return scalene_profiler.Scalene.should_trace(s)
-
-
 def main() -> None:
     try:
         from scalene import scalene_profiler
